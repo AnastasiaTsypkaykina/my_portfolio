be388ca4144c85c2b3b4def50e85d54ea004a048
Anastasia Tsypkaykina <tsypkaykina.anastasia@gmail.com>
2025-11-05 21:51:39 +0100

    responsive chnages

--- my_portfolio/my_portfolio/src/app/main-content/hero/components/title/title.ts
+++ my_portfolio/my_portfolio/src/app/main-content/hero/components/title/title.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-title',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './title.html',
   styleUrl: './title.scss'
 })
 export class Title {
+  constructor(private translate: TranslateService) {
+  }
 
 }
