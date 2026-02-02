196447ee40ea1369f17f5d996207614f4e2c4270
Anastasia Tsypkaykina <tsypkaykina.anastasia@gmail.com>
2026-02-02 13:30:34 +0100

    privacy close icon hingefügt

--- my_portfolio/my_portfolio/src/app/privacy-policy/privacy-policy.ts
+++ my_portfolio/my_portfolio/src/app/privacy-policy/privacy-policy.ts
@@ -1,14 +1,24 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy-policy',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss'
 })
 export class PrivacyPolicy {
-  constructor(public translate: TranslateService) { }
+  constructor(
+    public translate: TranslateService,
+    private location: Location,
+    private router: Router
+  ) {}
+
+ close(): void {
+  this.router.navigate(['./'], { fragment: 'contact' });
+}
 
 }
